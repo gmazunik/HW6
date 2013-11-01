042edb531beebd13cd23ba5eb05f2a3d37122281
Greg Mazunik <gmazunik@gmail.com>
2013-10-31 22:19:32 -0500

    Added login file and page

---
+++ login/default.aspx.vb
@@ -0,0 +1,9 @@
+﻿
+Partial Class login_default
+    Inherits System.Web.UI.Page
+
+    'Cursor in username box on page load
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Login1.Focus()
+    End Sub
+End Class
